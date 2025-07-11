78871a49e5ee99d1a19d8d1bfc0e64d96b4119e1
Sommmmy <59640424+Sommmmy@users.noreply.github.com>
2025-07-12 01:24:42 +0300

    Update SharePlugin.java

--- share/android/src/main/java/com/capacitorjs/plugins/share/SharePlugin.java
+++ share/android/src/main/java/com/capacitorjs/plugins/share/SharePlugin.java
@@ -41,7 +41,11 @@ public class SharePlugin extends Plugin {
                     }
                 }
             };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getActivity().registerReceiver(broadcastReceiver, new IntentFilter(Intent.EXTRA_CHOSEN_COMPONENT), Context.RECEIVER_NOT_EXPORTED);
+      } else {
         getActivity().registerReceiver(broadcastReceiver, new IntentFilter(Intent.EXTRA_CHOSEN_COMPONENT));
+      }
     }
 
     @SuppressWarnings("deprecation")
